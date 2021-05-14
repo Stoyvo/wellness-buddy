e828e1b75f5642ff5838f1903d883f0122fd2fab
Stoyvo <Stoyvo@users.noreply.github.com>
2021-05-14 16:05:28 -0400

    Animate Breathing

--- panel/breathingexercises/breathingexercises.go
+++ panel/breathingexercises/breathingexercises.go
@@ -2,8 +2,12 @@ package breathingexercises
 
 import (
 	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
+	"github.com/stoyvo/wellness-buddy/assets"
 	"net/url"
+	"time"
 )
 
 var Active bool
@@ -22,7 +26,7 @@ func Load(app fyne.App, content *fyne.Container) []fyne.CanvasObject {
 	objs := fetchDefaultObjs()
 
 	//uncomment for Breathing Exercise Demo
-	//Active = true
+	Active = true
 
 	if Active {
 		objs = append(objs, widget.NewButton("Done!", func() {
@@ -44,6 +48,28 @@ func Load(app fyne.App, content *fyne.Container) []fyne.CanvasObject {
 		content.Objects = objs
 		content.Layout.Layout(content.Objects, content.Size())
 	}
+
+	imgContainer := container.NewCenter()
+	var imgFrames = []*canvas.Image{
+		canvas.NewImageFromResource(assets.ResourceBreathe1Png),
+		canvas.NewImageFromResource(assets.ResourceBreathe2Png),
+	}
+	totalFrames := len(imgFrames)
+	go func() {
+		var i int
+		for {
+			if i >= totalFrames {
+				i=0
+			}
+			imgFrames[i].FillMode = canvas.ImageFillOriginal
+			imgContainer.Objects = []fyne.CanvasObject{imgFrames[i]}
+			imgContainer.Layout.Layout(imgContainer.Objects, imgContainer.Size())
+			content.Refresh()
+			time.Sleep(1 * time.Second)
+			i++
+		}
+	}()
+	objs = append(objs, imgContainer)
 
 	return objs
 }

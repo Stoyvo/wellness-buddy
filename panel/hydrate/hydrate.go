4d5f0c5f90e1c74bad1a90202e15a184746bd537
Ksenia <ksenia.zlotin@bounteous.com>
2021-05-14 15:11:46 -0400

    added resetting of panel when done or skip are clicked

--- panel/hydrate/hydrate.go
+++ panel/hydrate/hydrate.go
@@ -7,17 +7,29 @@ import (
 
 var Active bool
 
-func Load(app fyne.App, content *fyne.Container) []fyne.CanvasObject {
+func fetchDefaultObjs() []fyne.CanvasObject{
 	var objs []fyne.CanvasObject
 
 	objs = append(objs, widget.NewLabel("Remember to Hydrate"))
 
+	return objs
+}
+
+func Load(app fyne.App, content *fyne.Container) []fyne.CanvasObject {
+	objs := fetchDefaultObjs()
+
 	if Active {
 		objs = append(objs, widget.NewButton("Done!", func() {
 			//add a point to the user and reset the panel
+			objs = fetchDefaultObjs()
+			content.Objects = objs
+			content.Layout.Layout(content.Objects, content.Size())
 		}))
 		objs = append(objs, widget.NewButton("Skip", func() {
-			//dismiss the action by reseting the panel
+			//dismiss the action by resetting the panel
+			objs = fetchDefaultObjs()
+			content.Objects = objs
+			content.Layout.Layout(content.Objects, content.Size())
 		}))
 	}
 
